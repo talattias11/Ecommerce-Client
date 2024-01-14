0e08fa7f6120c51add9a4e243d35cb55ee71896c
Tal Attias <tala585@gmail.com>
2024-01-14 20:08:13 +0200

    init client side routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,23 @@
-export default function App(){
-  return 'Tal Attias'
+import { Route, Routes } from "react-router-dom";
+
+export default function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Catalog />} />
+        <Route path="/products/:id" element={<ProductPage />} />
+      </Routes>
+    </>
+  );
+}
+
+function Home(params) {
+  return 'home'
+}
+function Catalog(params) {
+  return 'catalog'
+}
+function ProductPage() {
+  return 'product page'
 }
