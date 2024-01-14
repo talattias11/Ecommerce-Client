483e8f297f044e80529d27e97d0c258435dd3fbb
Tal Attias <tala585@gmail.com>
2024-01-14 21:46:02 +0200

    starting numbers pagination

--- src/Components/Catalog.jsx
+++ src/Components/Catalog.jsx
@@ -1,19 +1,30 @@
-import { useState } from "react"
-import {products} from "../products"
-import ProductCard from "./ProductCard"
+import { useState } from "react";
+import { products } from "../products";
+import ProductCard from "./ProductCard";
 
 const itemsPerPage = 6;
 
 export default function Catalog() {
-  const [curPageNum , setCurPageNum] = useState(1)
+  const [curPageNum, setCurPageNum] = useState(1);
 
-  const firstIndex = (curPageNum-1) * itemsPerPage;
+  const firstIndex = (curPageNum - 1) * itemsPerPage;
   const seconedIndex = firstIndex + itemsPerPage;
-  
-  const productsCards = products.slice(firstIndex, seconedIndex).map(p => 
-  <ProductCard key={p.id} product={p}/>)
 
-  return <div className="catalog-board">
-      <ul>{productsCards}</ul>
+  const productsCards = products
+    .slice(firstIndex, seconedIndex)
+    .map((p) => <ProductCard key={p.id} product={p} />);
+
+  return <>
+      <div className="catalog-board">{productsCards}</div>
+      <CatalogPagesLinks selectedPage={curPageNum} />
+    </>
+}
+
+function CatalogPagesLinks({selectedPage , products}) {
+      return <div className="pages-links">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
       </div>
-  }
+  
+}
