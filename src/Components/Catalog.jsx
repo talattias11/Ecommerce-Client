1c3af9c25fd18465148db757c3714693f90358de
Tal Attias <tala585@gmail.com>
2024-01-16 22:08:09 +0200

    sort feture

--- src/Components/Catalog.jsx
+++ src/Components/Catalog.jsx
@@ -4,14 +4,41 @@ import ProductCard from "./ProductCard";
 import CatalogPagesLinks from "./CatalogPagesLinks";
 
 const itemsPerPage = 6;
-const pagesCount = Math.ceil(products.length / itemsPerPage);
+
+const sortCategories = ['name', 'price'];
 
 export default function Catalog() {
+  const [searchedProducts, setSearchedProducts] = useState(products);
   const [curPageNum, setCurPageNum] = useState(1);
+  const cards = getPageCards(curPageNum, searchedProducts);
 
-  const cards = getPageCards(curPageNum);
+  function handleSearch(str) {
+    const result = products.filter(p =>
+      p.name.includes(str));
+    setSearchedProducts(result);
+  }
+
+  function handleSort(category) {
+    const productsClone = [...products];
+
+    switch (category) {
+      case 'name':
+        productsClone.sort((p1, p2) => p1.name.localeCompare(p2.name));
+        break;
+      case 'price':
+        productsClone.sort((p1, p2) => p1.price - p2.price);
+        break;
+    }
+
+    setSearchedProducts(productsClone);
+  }
+
+  const pagesCount = Math.ceil(searchedProducts.length / itemsPerPage);
 
   return <>
+    <CatalogControls
+      handleSort={handleSort}
+      handleSearch={handleSearch} />
     <CatalogBoard cards={cards} />
     <CatalogPagesLinks selectedPageNum={curPageNum}
       onPageBtnClick={num => setCurPageNum(num)}
@@ -19,11 +46,31 @@ export default function Catalog() {
   </>;
 }
 
-function getPageCards(pageNum) {
+function CatalogControls({ handleSearch, handleSort }) {
+  const [inputVal, setInputVal] = useState('');
+  
+  const sortOptions = ['', ...sortCategories]
+    .map(c => <option key={c} value={c}>
+      {c}
+    </option>);
+
+  return <div className="catalog-board-controls">
+    <input type="text" value={inputVal}
+      onChange={e => setInputVal(e.target.value)} />
+    <button onClick={() => handleSearch(inputVal)}>
+      search
+    </button>
+    <select onChange={e => handleSort(e.target.value)}>
+      {sortOptions}
+    </select>
+  </div>
+}
+
+function getPageCards(pageNum, allProducts) {
   const firstproductIndex =
     (pageNum - 1) * itemsPerPage;
-  
-  const pageProducts = products.slice(firstproductIndex,
+
+  const pageProducts = allProducts.slice(firstproductIndex,
     firstproductIndex + itemsPerPage)
 
   return pageProducts.map(p =>
